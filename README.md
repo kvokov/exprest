2568823d74759d1a4c5b1272d6f725d869dff7d5
DK <kvokovdl@gmail.com>
2016-11-13 04:20:33 +0200

    fixed utracked files

--- README.md
+++ README.md
@@ -4,6 +4,114 @@ ExpREST helps you build RESTful API with [Express](http://expressjs.com/) and [B
 
 Try on example - [exprest-example](https://github.com/kvokov/exprest-example)
 
+## Installation
+```bash
+$ npm install exprest --save
+```
+
+## Quick Start
+
+Lets say you have a Bookshelf model called `User`
+
+```javascript
+let User = bookshelf.Model.extend({
+    tableName: 'users'
+});
+```
+
+To create CRUD endpoints for your models you just need to pass resources array to ExpREST
+
+```javascript
+let app = require('express')();
+let exprest = require('exprest');
+
+app.use(exprest({resources: [User]}));
+
+app.listen(3000);
+```
+
+And you gets these endpoints
+
+| Method | URL          | Action                  |
+| :----- | :------------| :-----------------------|
+| GET    | `/users`     | Get list of users       |
+| GET    | `/users/:id` | Get a user by id        |
+| POST   | `/users`     | Create a new user       |
+| PUT    | `/users/:id` | Update an existing user |
+| DELETE | `/users/:id` | Delete an existing user |
+
+**That's it!** :sunglasses:
+
+## Documentation
+
+- [Configuration](#configuration)
+- [Endpoints](#endpoints)
+- [Hooks](#hooks)
+- [Examples](#examples)
+
+### Configuration
+
+### Endpoints
+
+#### Get list of entities
+
+> GET `/entities`
+
+Listing resources support filtering, searching, sorting, and pagination.
+
+#### Get an entity by identifier
+
+> GET `/entities/:identifier`
+
+#### Create a new entity
+
+> POST `/entities`
+
+#### Update an existing entity
+
+> PUT `/entities/:identifier`
+
+#### Delete an existing entity
+
+> DELETE `/entities/:identifier`
+
+### Hooks
+
+Hooks let you extend and override default endpoint behaviors.
+
+`before` hooks are executed before the default database action, such as fetch, save, or delete. 
+
+`after` hooks are executed after all database actions are complete.
+
+Hook names are generated based on endpoint configurations. This list is based on a `/users` endpoint where `itemName = user` and `collectionName = users`
+
+| Hook Name                 | Request                   | Arguments                             |
+| :-------------------------| :------------------------ | :------------------------------------ |
+| `beforeGetUsers`          | GET `/users`              | [req, res, userModel]                 |
+| `afterGetUsers`           | GET `/users`              | [req, res, userCollection]            |
+| `beforeGetUser`           | GET `/users/:id`          | [req, res, userModel]                 |
+| `afterGetUser`            | GET `/users/:id`          | [req, res, userModel]                 |
+| `beforeCreateUser`        | POST `/users`             | [req, res, userModel]                 |
+| `afterCreateUser`         | POST `/users`             | [req, res, userModel]                 |
+| `beforeUpdateUser`        | PUT `/users/:id`          | [req, res, userModel]                 |
+| `afterUpdateUser`         | PUT `/users/:id`          | [req, res, userModel]                 |
+| `beforeDeleteUser`        | DELETE `/users/:id`       | [req, res, userModel]                 |
+| `afterDeleteUser`         | DELETE `/users/:id`       | [req, res, userModel]                 |
+| `beforeGetRelatedThings`  | GET `/users/:id/things`   | [req, res, thingModel]                |
+| `afterGetRelatedThings`   | GET `/users/:id/things`   | [req, res, thingsCollection]          |
+| `beforeRelatedThing`      | POST `/users/:id/things`  | [req, res, userModel]                 |
+| `afterRelateThing`        | POST `/users/:id/things`  | [req, res, userModel, thingModel]     |
+
+`req` and `res` are an Express [request](http://expressjs.com/4x/api.html#request) and [response](http://expressjs.com/4x/api.html#response)
+
+`userModel` is an instance of a [bookshelf model](http://bookshelfjs.org/#Model)
+
+`userCollection` is an instance of a [bookshelf collection](http://bookshelfjs.org/#Collection)
+
+#### Adding hooks
+
+#### How to use hooks
+
 ## License
 
 [MIT](LICENSE)
--- src/index.js
+++ src/index.js
@@ -2,14 +2,180 @@ import { Router } from 'express';
 import bodyParser from 'body-parser';
 
 
+const DEFAULT_ITEMS_PER_PAGE = 50;
+let config = {
+    pagination: {
+        itemsPerPage: DEFAULT_ITEMS_PER_PAGE,
+    },
+    resources: false
+};
 let router;
 
 
 function init(options = {}) {
+    Object.assign(config, options);
+
     router = new Router();
     router.use(bodyParser.json());
-    
+    //router.createResource = createResource;
+
+    if(Array.isArray(config.resources)) {
+        config.resources = config.resources.map(normalizeResource);
+        config.resources.forEach(createApi);
+    }
+
     return router;
+}
+
+
+function normalizeResource(resourceConfig) {
+    let resource = (typeof resourceConfig !== 'object') ? {model: resourceConfig} : resourceConfig;
+
+    resource.collectionName = resource.collectionName || resource.model.name.toLowerCase() + 's';
+    resource.itemName       = resource.itemName || resource.model.name.toLowerCase();
+    resource.identifier     = resource.identifier || resource.model.forge().idAttribute;
+
+    let hooksArray = {
+        getItem         : [],
+        getCollection   : [],
+        createItem      : [],
+        updateItem      : [],
+        deleteItem      : []
+    };
+    resource.hooks = {
+        before: {...hooksArray},
+        after: {...hooksArray}
+    };
+
+    return resource;
+}
+
+
+function createApi(resource) {
+    createEndpoints(resource);
+    createHookSetters(resource);
+
+    return router;
+}
+
+
+function createEndpoints(resource) {
+    router.route('/' + resource.collectionName)
+        .get(createCollectionEndpoint(resource));
+
+    /*
+     router.route('/' + resource.endpoint + '/:id')
+     .get(createItemEndpoint(resource));
+     */
+}
+
+
+function createHookSetters(resource) {
+    router['beforeGet' + capitalize(resource.collectionName)] = createHook(resource, 'before', 'getCollection');
+    router['beforeGet' + capitalize(resource.itemName)] = createHook(resource, 'before', 'getItem');
+    router['beforeCreate' + capitalize(resource.itemName)] = createHook(resource, 'before', 'createItem');
+    router['beforeUpdate' + capitalize(resource.itemName)] = createHook(resource, 'before', 'updateItem');
+    router['beforeDelete' + capitalize(resource.itemName)] = createHook(resource, 'before', 'deleteItem');
+
+    router['afterGet' + capitalize(resource.collectionName)] = createHook(resource, 'after', 'getCollection');
+    router['afterGet' + capitalize(resource.itemName)] = createHook(resource, 'after', 'getItem');
+    router['afterCreate' + capitalize(resource.itemName)] = createHook(resource, 'after', 'createItem');
+    router['afterUpdate' + capitalize(resource.itemName)] = createHook(resource, 'after', 'updateItem');
+    router['afterDelete' + capitalize(resource.itemName)] = createHook(resource, 'after', 'deleteItem');
+}
+
+function createHook(resource, time, name) {
+    return (fn) => {
+        if(typeof fn !== 'function') {
+            throw new Error('Hook should be a function.')
+        }
+        resource.hooks[time][name].push(fn);
+    }
+}
+
+
+function execHooks(resource, time, name, args) {
+    console.log('hooks ' + time + ' ' + name, resource.hooks[time][name]);
+    //console.log(args);
+}
+
+//
+// Endpoints creators --------------------------------------------------------------------------------------------------
+//
+
+
+function createCollectionEndpoint(resource) {
+    return (req, res, next) => {
+
+        execHooks(resource, 'before', 'getCollection', [req, res]);
+
+        console.log('endpoint')
+        let params = {...req.query};
+        if(config.pagination) {
+            params.page = params.page || 1;
+            params.size = params.size || config.pagination.itemsPerPage || DEFAULT_ITEMS_PER_PAGE;
+        }
+
+        params.sort   = params.sort || resource.identifier;
+        params.expand  = params.expand ? (req.query.expand).split(',') : [];
+
+        let criteria = {};
+
+        Promise.all([
+                // get items count
+                new resource.model()
+                    .where(criteria)
+                    .count(),
+                // get items
+                new resource.model()
+                    .query((qb) => {
+                        if(config.pagination) {
+                            qb.where(criteria).offset((params.page - 1) * params.size).limit(params.size);
+                        }
+                    })
+                    .orderBy(params.sort)
+                    .fetchAll({withRelated: params.expand})
+            ])
+            .then((results) => {
+                let totalItemsCount = results[0] || 0;
+                let items = results[1];
+
+                let headers = {
+                    'x-total-count': totalItemsCount
+                };
+                if(config.pagination) {
+                    headers['x-page']       = params.page;
+                    headers['x-page-size']  = params.size;
+                }
+
+                execHooks(resource, 'after', 'getCollection', [req, res]);
+
+                res.set(headers).send(items);
+            })
+            .catch(next);
+    }
+}
+
+function createItemEndpoint(resource) {
+    return (req, res, next) => {
+        let criteria = {};
+        criteria[resource.identifier] = req.params.id;
+
+        new resource.model()
+            .where(criteria)
+            .fetch()
+            .then((item) => {
+                if(!item) {
+                    return next(new Error('Not Found', 404));
+                }
+                res.json(item);
+            })
+            .catch(next);
+    }
+}
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 
